ca23178d3d0569a5e8bb99a69255aca398922c7b
shohdmax <shohdmax@gmail.com>
2022-05-26 19:01:58 +0600

    git test

--- src/Component/Pages/Myprofile/Myprofile.js
+++ src/Component/Pages/Myprofile/Myprofile.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Myprofile = () => {
+    const [user] = useAuthState(auth);
     return (
-        <div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+
+                </div>
+                <div className='col-md-4'>
+                <div class="card w-100">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">User Name : {user.displayName}</li>
+                      <li class="list-group-item">Email : {user.email}</li>
+                      <li class="list-group-item">A third item</li>
+                    </ul>
+                </div>
+
+                </div>
+                <div className='col-md-4'>
+
+                </div>
+                
+            </div>
             
         </div>
     );
